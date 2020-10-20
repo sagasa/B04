1d8c9a3068556d230f90c1b012f2b89c843d82c5
Nakamura <gc19g1391@gmail.com>
2020-10-20 12:12:05 +0900

    色々設定。

--- SurogSakones.cpp
+++ SurogSakones.cpp
@@ -3,45 +3,33 @@
 #include"Field.h";
 #include"Player.h"
 
-SurogSakones::SurogSakones(IWorld* world,const GSvector3& position){
+SurogSakones::SurogSakones(IWorld* world, const GSvector3& position) {
 	world_ = world;
 	tag_ = "EnemyTag";
 	name_ = "SurogSakones";
 	transform_.position(position);
 	collider_ = BoundingSphere(5);
-    state_ = State::Normal;
-    hp_ = 100.0f;
+	state_ = State::Normal;
+	hp_ = 100.0f;
+
+	move_pos_.push_back(transform().position());
+	move_pos_.push_back(transform().position() - GSvector3{ 50.0f,0.0f,0.0f });
 }
-void SurogSakones::update(float delta_time){
-    if (hp_ <= 0)die();
-    GSvector3 inputVelocity{ 0.0f, 0.0f, 0.0f };
-    if (gsGetKeyState(GKEY_LEFT) == GS_TRUE) {
-        inputVelocity.x = -1.0f;
-    }
-    if (gsGetKeyState(GKEY_RIGHT) == GS_TRUE) {
-        inputVelocity.x = 1.0f;
-    }
-    if (gsGetKeyState(GKEY_UP) == GS_TRUE) {
-        inputVelocity.y = 1.0f;
-    }
-    if (gsGetKeyState(GKEY_DOWN) == GS_TRUE) {
-        inputVelocity.y = -1.0f;
-    }
-    // 移動量を計算
-    float speed = 1.0f;    // 移動スピード
-    velocity_ = inputVelocity.normalized() * speed * delta_time;
-    // 座標を取得
-    GSvector3 position = transform_.position();
-    // 座標に移動量を加算
-    position += velocity_;
-    // 座標の設定
-    transform_.position(position);
+void SurogSakones::update(float delta_time) {
+	if (hp_ <= 0)die();
+	move(delta_time);	
+	if (gsGetKeyState(GKEY_UP) == GS_TRUE) {
+		destination_ = move_pos_[0];
+	}
+	if (gsGetKeyState(GKEY_DOWN) == GS_TRUE) {
+		destination_ = move_pos_[1];
+	}
 }
-void SurogSakones::draw()const{
+void SurogSakones::draw()const {
 	glPushMatrix();
 	glMultMatrixf(transform_.localToWorldMatrix());
-    collider().draw();
 	glPopMatrix();
+	collider().draw();
 }
 
 /**
@@ -49,20 +37,34 @@ void SurogSakones::draw()const{
 * 改良予定
 */
 void SurogSakones::Damage() {
-    hp_ -= 10;
+	hp_ -= 10;
 }
 
 void SurogSakones::pshychokinesis(const GSvector3& position) {
-    Actor* player = world_->find_actor("Player");
-    if (player != nullptr) {
-        GSvector3 to_player = (player->transform().position() - transform().position()).normalized();
-        
-    }
+	Actor* player = world_->find_actor("Player");
+	if (player != nullptr) {
+		//プレイヤーのベクトルを求める
+		GSvector3 to_player = (player->transform().position() - transform().position()).normalized();
+		//球を出す処理
+	}
 }
-void SurogSakones::move() {
-
+void SurogSakones::move(float delta_time) {
+	if (GSvector3::distance(transform_.position(), destination_) <= 0.5f) {
+		transform_.position(destination_);
+		enable_collider_ = true;
+	}
+	else if (transform_.position() < destination_) {
+		velocity_ = GSvector3{ 1.0f,0.0f,0.0f };
+		transform_.translate(velocity_ * delta_time);
+		enable_collider_ = false;
+	}
+	else if (transform_.position() > destination_) {
+		velocity_ = GSvector3{ -1.0f,0.0f,0.0f };
+		transform_.translate(velocity_ * delta_time);
+		enable_collider_ = false;
+	}
 }
 
-void SurogSakones::move_attack() {
+void SurogSakones::move_attack(float delta_time) {
 
 }
--- SurogSakones.h
+++ SurogSakones.h
@@ -4,33 +4,37 @@
 class IWorld;
 
 #include "Actor.h"
+#include<vector>
+
 class SurogSakones :
-    public Actor
+	public Actor
 {
 public:
-    enum class State {
-        Unkown,
-        Apper,
-        Normal,
-        Angry
-    };
+	enum class State {
+		Unkown,
+		Apper,
+		Normal,
+		Angry
+	};
 public:
-    SurogSakones(IWorld* world, const GSvector3& position);
-    virtual void update(float delta_time)override;
-    virtual void draw()const override;
-    void Damage();
-
-private:    
-    //”O“®UŒ‚
-    void pshychokinesis(const GSvector3& position);
-    //•’Ê‚ÌˆÚ“®(”½‘Î‘¤‚ÉˆÚ“®‚·‚é)
-    void move();
-    //ˆÚ“®‚µ‚Â‚ÂUŒ‚
-    void move_attack();
+	SurogSakones(IWorld* world, const GSvector3& position);
+	virtual void update(float delta_time)override;
+	virtual void draw()const override;
+	void Damage();
 
 private:
-    State state_;
-    float hp_{ 0.0f };
+	//”O“®UŒ‚
+	void pshychokinesis(const GSvector3& position);
+	//•’Ê‚ÌˆÚ“®(”½‘Î‘¤‚ÉˆÚ“®‚·‚é)
+	void move(float delta_time);
+	//ˆÚ“®‚µ‚Â‚ÂUŒ‚
+	void move_attack(float delta_time);
+
+private:
+	State state_ = State::Unkown;
+	float hp_{ 0.0f };
+	std::vector<GSvector3> move_pos_;
+	GSvector3 destination_;
 
 };
 
