b143150a548f5b154267feee7793253e4503c028
Nakamura <gc19g1391@gmail.com>
2021-01-25 19:09:09 +0900

    編集。

--- BossTestScene.cpp
+++ BossTestScene.cpp
@@ -41,11 +41,11 @@ void BossTestScene::start() {
     gsLoadAnimation(Animation_Paladin, "Assets/Model/Paladin/Paladin.anm");
 
 	//SE
-    gsLoadSE(SE_GhostDamage, "Assets/SE/ghost_damage.wav",1, false);
-    gsLoadSE(SE_GhostAttack1, "Assets/SE/Ghost Attack_01.wav",1, false);
-    gsLoadSE(SE_GhostAttack2, "Assets/SE/Ghost Attack_02.wav", 1, false);
-    gsLoadSE(SE_BossGhostDamage, "Assets/SE/Ghost Damaged_01.wav", 1, false);
-    gsLoadSE(SE_GhostDeath, "Assets/SE/Ghost Death_02.wav", 1, false);
+    gsLoadSE(SE_GhostDamage, "Assets/SE/ghost_damage.wav",1, GWAVE_DEFAULT);
+    gsLoadSE(SE_GhostAttack1, "Assets/SE/Ghost Attack_01.wav",1, GWAVE_DEFAULT);
+    gsLoadSE(SE_GhostAttack2, "Assets/SE/Ghost Attack_02.wav", 1, GWAVE_DEFAULT);
+    gsLoadSE(SE_BossGhostDamage, "Assets/SE/Ghost Damaged_01.wav", 1, GWAVE_DEFAULT);
+    gsLoadSE(SE_GhostDeath, "Assets/SE/Ghost Death_02.wav", 1, GWAVE_DEFAULT);
     
     //SurogSakonesÇÃÉÅÉbÉVÉÖÇÃì«Ç›çûÇ›
     gsLoadMesh(Mesh_SurogSakones, "Assets/Model/Enemy/Ghost_T-pose.msh");
