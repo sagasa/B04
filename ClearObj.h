c3ac19bdb08cff5009e83d6fdc449cdea7f15e13
SAGASA_main <sagasa.ota@gmail.com>
2021-02-08 16:00:30 +0900

    触れるとクリアするオブジェクト追加

--- ClearObj.h
+++ ClearObj.h
@@ -1,6 +1,29 @@
 #pragma once
-class ClearObj
-{
-	
+#include"Actor.h"
+#include "World.h"
+class ClearObj :public Actor {
+public:
+    //コンストラクタ
+    ClearObj(IWorld* world, const GSvector3& position)
+    {
+        world_ = world;
+        name_ = "ClearFlag";
+        tag_ = "ClearTag";
+        collider_ = BoundingSphere{ 2 };
+        enable_collider_ = true;
+    }
+    // 描画
+    void draw() const override
+    {
+        collider().draw();
+    }
+
+    void collide(Actor& other) override
+    {
+        if (other.tag() == "PlayerTag")
+        {
+            world_->game_clear();
+        }
+    }
 };
 
