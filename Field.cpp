e9cb6fdbae484e4b08e557167a2e4667c7d9139d
Nakamura <gc19g1391@gmail.com>
2021-02-08 16:27:19 +0900

    フィールド変更。

--- Field.cpp
+++ Field.cpp
@@ -20,12 +20,12 @@ void Field::draw() const {
 }
 
 bool Field::collide(const Line& line, GSvector3* intersect, GSplane* plane)const {
-    return gsOctreeCollisionLine(gsGetOctree(octree_), &line.start, &line.end, intersect, plane);
+    return gsOctreeCollisionLine(gsGetOctree(collider_), &line.start, &line.end, intersect, plane);
 }
 bool Field::collide(const Ray& ray, float max_distance, GSvector3* intersect, GSplane* plane)const {
     Line line{ ray.position,ray.position + (ray.direction.normalized() * max_distance) };
     return collide(line, intersect, plane);
 }
 bool Field::collide(const BoundingSphere& sphere, GSvector3* center)const {
-    return gsOctreeCollisionSphere(gsGetOctree(0), &sphere.center, sphere.radius, center);
+    return gsOctreeCollisionSphere(gsGetOctree(collider_), &sphere.center, sphere.radius, center);
 }
